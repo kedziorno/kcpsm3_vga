9769b3c7f8529da1c6aa64146cd0ffade64b5b34
kedziorno <kjoblak85@gmail.com>
2025-01-25 19:09:36 +0100

    program.psm - cordic 8bit, fix x=k, we have quarter circle, compare 360 ang, quadrants4, lower half circle, after fix 1 quadrant,test5 - 3/4 quarter circle

--- top.vhd
+++ top.vhd
@@ -239,7 +239,7 @@ begin
 --        v_sin_v := kcpsm3_out_port & v_sin_v (15 downto 8); -- LO first
         v_sin_v := kcpsm3_out_port; -- LO first
 --        if (flag = true) then
-          v_sin_r := real (to_integer (unsigned (v_sin_v)));
+          v_sin_r := real (to_integer (signed (v_sin_v)));
           s_sin_v <= v_sin_v;
           v_sin_r := v_sin_r / factor;
           --report "sin_cordic " & real'image (v_sin_r);
@@ -253,7 +253,7 @@ begin
 --        v_cos_v := kcpsm3_out_port & v_cos_v (15 downto 8); -- LO first
         v_cos_v := kcpsm3_out_port; -- LO first
 --        if (flag = true) then
-          v_cos_r := real (to_integer (unsigned (v_cos_v)));
+          v_cos_r := real (to_integer (signed (v_cos_v)));
           s_cos_v <= v_cos_v;
           v_cos_r := v_cos_r / factor;
           --report "cos_cordic " & real'image (v_cos_r);
